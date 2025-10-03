7e99f52fdf4c7c492092ebd4b8bb130175f3aa37
Wiacl <wasewero@yandex.ru>
2025-10-03 20:17:30 +0300

    3_4lab

--- lab3/lab3_all.py
+++ lab3/lab3_all.py
@@ -7,3 +7,20 @@ print(even_numbers)
 words = ["python", "Java", "c++", "Rust", "go"]
 result = [word.upper() for word in words if len(word) > 3]
 print(result)
+
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+    
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.n == 0:
+            raise StopIteration
+        current = self.n
+        self.n -= 1
+        return current
+
+for x in Countdown(5):
+    print(x)
