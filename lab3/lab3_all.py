be04f88aaf798407ae76a31af342843caa814d32
Wiacl <wasewero@yandex.ru>
2025-10-03 20:16:45 +0300

    3_3lab

--- lab3/lab3_all.py
+++ lab3/lab3_all.py
@@ -3,3 +3,7 @@ print(squares)
 
 even_numbers = [x for x in range(1, 20) if x % 2 == 0]
 print(even_numbers)
+
+words = ["python", "Java", "c++", "Rust", "go"]
+result = [word.upper() for word in words if len(word) > 3]
+print(result)
