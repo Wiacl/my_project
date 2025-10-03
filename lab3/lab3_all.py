1029dd865ee2236a10efb5ca069342c4a3f598a0
Wiacl <wasewero@yandex.ru>
2025-10-03 20:17:46 +0300

    3_5alb

--- lab3/lab3_all.py
+++ lab3/lab3_all.py
@@ -24,3 +24,13 @@ class Countdown:
 
 for x in Countdown(5):
     print(x)
+    
+
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+for num in fibonacci(5):
+    print(num)
